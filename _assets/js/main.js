97c1278557ac96ccae58854a03cb6a9bfd734f55
Birgit <birgit.voslamber@secretescapes.com>
2019-07-08 14:44:51 +0200

    content update

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -12,7 +12,7 @@ $(window).on('scroll', function () {
          //remove the background property so it comes transparent again (defined in your css)
         $(".bv-title-yellow").removeClass("text--yellow");
      }
-  //console.log('hey');
+
 var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
     var pixs = $(document).scrollTop(),
@@ -23,9 +23,40 @@ var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
     //console.log(calc+' '+pixs);
 
 	if(isFirefox === false){
-    	$(".bv-banner-out").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)","opacity": calc });
-      //$(".bv-title-yellow").addClass("text--yellow");
-  }  else { $(".bv-banner-out").css({"opacity": calc });}
+    $(".bv-banner-out").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)","opacity": calc });
+  } else { $(".bv-banner-out").css({"opacity": calc }); }
 });
 
-//console.log('hey');
+
+
+// Get the modal
+var modal = document.getElementById('bv-myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("bv-myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("bv-close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+    $('body').addClass('bv-disable-scroll');
+		$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+    $('body').removeClass('bv-disable-scroll');
+		$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+      $('body').removeClass('bv-disable-scroll');
+			$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+  }
+}
